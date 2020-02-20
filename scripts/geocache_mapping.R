06495f6825949d9d502bdfdd2fbb108716167acd
AdrianForsythe <adrian.e.forsythe@gmail.com>
2020-02-19 20:43:07 -0500

    added a few new figures, fiddled with glm and spatial weights. lagsarlm not yet ready

--- scripts/geocache_mapping.R
+++ scripts/geocache_mapping.R
@@ -11,18 +11,23 @@ mapping <- function (...) {
                   summarise(total = length(User))
   
   register_google(key = "AIzaSyDw5appsfJ_gWd45-AeYe_WTT2VvI8kXhQ")
-  map <- get_map(c(right = -57, left = -96, top = 52, bottom = 32), 
+  map <- get_stamenmap(c(right = -57, left = -96, top = 52, bottom = 32), 
                  zoom = 5, source = "stamen", maptype = "terrain-background", scale = "auto")
   n <- length(unique(all_summary$Year))
   qual_col_pals = brewer.pal.info[brewer.pal.info$category == "qual", ]
   col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
   
-  ggmap(map) + 
+  ggplot()+
+    borders("world") +
+    geom_sf(aes(geometry=presence.df$geoms))+
     geom_path(data = all_visits_window, aes(y = coords.x2,x = coords.x1, group = User), color = "red", alpha = 0.3) +
-    geom_point(data = all_summary, aes(y = coords.x2, x = coords.x1, size = total), alpha = 0.25) + 
+    geom_point(data = all_summary, aes(y = coords.x2, x = coords.x1),color="black") +
+    # coord_sf(xlim = c(-125, -57.5), ylim = c(27.5, 55))+
+    coord_sf(xlim = c(-96, -57), ylim = c(32, 52))+
     theme_classic() + 
-    theme(axis.text = element_text(size = 10),axis.title = element_text(size = 12), legend.position = "none")
-  
+    theme(axis.text = element_text(size = 10), 
+          axis.title = element_text(size = 12),
+          legend.position = "none")
   ggsave(filename = "figures/num-geocache.png", plot = last_plot())
   
   num.geocache <- all_visits_window %>% group_by(Date) %>%
@@ -50,7 +55,9 @@ mapping <- function (...) {
   
   # summary(lmer(total ~ Date + (1 | GC), data = user.activity.date))
   
-  p <- ggmap(map) + geom_point(data = all_visits_window, aes(y = coords.x2, x = coords.x1, group = User)) + 
+  p <- ggmap(map) + 
+    geom_point(data = all_visits_window[which(all_visits_window$Year<2008),], aes(y = coords.x2, x = coords.x1, group = User),color="blue")+
+    geom_point(data = all_visits_window[which(all_visits_window$Year>2008),], aes(y = coords.x2, x = coords.x1, group = User),color="red")+ 
     transition_reveal(along = Date, keep_last = F) + shadow_wake(wake_length = 0.1, alpha = T) + 
     ggtitle("Year: {frame_along}") + 
     theme_classic() + 
--- scripts/lagsarlm.R
+++ scripts/lagsarlm.R
@@ -1,29 +1,5 @@
-
-# construct neighbour list from county centroids
-county.n <- poly2nb(uniq.poly, row.names = names(uniq.poly))
-
-# spatial weights of neighbourlist
-county.w <- nb2listw(county.n,style = "C",zero.policy = TRUE)
-lapply(county.w$weights, function(x) ifelse(x == "NULL",0,x))
-
-library(reshape)
-df <- data.frame(matrix(unlist(county.w$weights), nrow=length(county.w$weights), byrow=T))
-lapply(county.w$weights,function(x) ifelse(!is.numeric(x),0,melt(x)))
-melt<-melt.list(county.w$weights[1:65])
-
-# spatial weight matricies from neighbour list
-# county.m <- nb2mat(county.n, style = "B", zero.policy = T)
-
-updated<-presence.df[na.omit(unique(num)),]
-updated$WNS_MAP_YR <- ymd(gsub("-.+", "/01/01", updated$WNS_MAP_YR))
-updated$YR_SUSPECT <- ymd(gsub("-.+", "/01/01", updated$YR_SUSPECT))
-updated$YR_CONFIRM <- ymd(gsub("-.+", "/01/01", updated$YR_CONFIRM))
-updated$incidence <- ifelse(is.na(updated$WNS_MAP_YR),0,updated$WNS_MAP_YR)
-
-updated$activity<-as.data.frame(z[match(updated$COUNTYNAME,z$county),"total"])$total
-
 ####
-
-lag = lagsarlm(incidence ~ activity + COUNTYNAME, data=updated, listw = county.w,tol.solve=1.0e-30, zero.policy=TRUE)
+lag = lagsarlm(as.factor(incidence) ~ num.shared + county1 + year,
+               data=all.shared.users, listw = county.w,tol.solve=1.0e-30, zero.policy=TRUE)
 
 summary(lag)
